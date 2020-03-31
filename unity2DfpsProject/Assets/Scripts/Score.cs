e1bd2778fe23179a95700bc79742813f3225fb8d
Tomas O'malley <g00361128@gmit.ie>
2020-03-31 16:33:17 +0100

    Added audio

---
+++ unity2DfpsProject/Assets/Scripts/Score.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Score : MonoBehaviour
+{
+
+    public Transform PlayerShip;
+    public Text scoreText;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = PlayerShip.position.z.ToString();
+    }
+}
