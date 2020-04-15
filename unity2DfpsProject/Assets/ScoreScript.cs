93bf37b3f59903c5fc4e4e6c9b2e52f96442ff2c
System Administrator <root@Tomass-iMac-Pro.local>
2020-04-15 13:03:46 +0100

    Added Silver Coin Annimation

--- unity2DfpsProject/Assets/ScoreScript.cs
+++ unity2DfpsProject/Assets/ScoreScript.cs
@@ -6,17 +6,13 @@ using UnityEngine.UI;
 public class ScoreScript : MonoBehaviour
 {
     
-	public static  int scoreValue = 0;
-     Text score;
     
 
     // Update is called once per frame
     void start()
     {
-    	score = GetComponent<Text> ();
     }
     void Update()
     {
-    	score.text = "Your Score:" + scoreValue;
     }
 }
--- unity2DfpsProject/Assets/Scripts/PlayerMovement.cs
+++ unity2DfpsProject/Assets/Scripts/PlayerMovement.cs
@@ -65,4 +65,18 @@ public class PlayerMovement : MonoBehaviour {
 
 
 	}
+
+
+
+private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("coins"))
+        {
+			Destroy(other.gameObject);
+        }
+    }
+
+
+
+
 }
